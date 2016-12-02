74a6587a66d10bbdc6856e19a95d43073af371ea
Martsynyuk <terr83@gmail.com>
2016-12-02 16:16:33 +0200

    1

--- app/Http/Controllers/TimeManageController.php
+++ app/Http/Controllers/TimeManageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class TimeManageController extends Controller
 {
