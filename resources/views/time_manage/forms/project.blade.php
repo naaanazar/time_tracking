bfefab3f4b91764c94ec4406942c94159fa03800
naaanazar <naaanazar>
2016-12-09 11:33:39 +0200

    minor change

--- resources/views/time_manage/forms/project.blade.php
+++ resources/views/time_manage/forms/project.blade.php
@@ -86,6 +86,23 @@
                                     @endif
                                 </div>
                             </div>
+                            <div class="control-group row">
+                                <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2" style="text-align: right;">
+                                    <label class="control-label" for="TeamLeaProjectId" style="text-align: left;">TeamLead</label>
+                                </div>
+                                <div class="controls col-xs-8 col-sm-6 col-md-5 col-lg-4">
+                                    <select name="company_id" class="input-xlarge focused my_input"  id="TeamLeaProjectId" style="height: 42px;">
+                                        <option selected>Please change Project</option>
+                                        @if (isset($client->company_name))
+                                            <option value="{{ $client->id }}" selected>{{ $client->company_name }}</option>
+                                        @endif
+
+                                        @foreach( $client as $key )
+                                            <option  value="{{ $key->id}}">{{ $key->company_name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
 
                             <div class="form-actions row">
                                 <label class="control-label col-sm-2" for=""></label>
