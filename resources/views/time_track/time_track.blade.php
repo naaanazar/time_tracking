4779270e80bda531a5452e13a6c41239c4c2e277
Nazar Korchevskiy <naaa.nazar@gmail.com>
2016-12-13 11:13:10 +0200

    fix form track

--- resources/views/time_track/time_track.blade.php
+++ resources/views/time_track/time_track.blade.php
@@ -43,54 +43,59 @@
 
             <div class="col-sm-6 col-md-6 col-lg-6" style="border-right: 1px solid #ccc; padding-top: 20px">
 
-                <form>
+                <form  method="POST" action="" id="addTrackForm">
+
                     <div class="form-group form-group-edit col-xs-12 col-sm-12 col-md-12 col-lg-12" >
                         <div class="col-xs-2 col-sm-4 col-md-3 col-lg-3" style="text-align: right;">
-                            <label class="control-label labelTrack" for="taskProjectId">Project</label>
+                            <label class="control-label labelTrack" for="trackProjectId">Project</label>
                         </div>
                         <div class="controls col-xs-12 col-sm-8 col-md-9 col-lg-9">
 
-                                <select name="project_id" class="inputTrackPadding focused my_input"  id="taskProjectId" style="height: 35px;" required>
+                                <select name="projects" class="inputTrackPadding focused my_input"  id="trackProjectId" style="height: 35px;" required>
                                 </select>
                         </div>
                     </div>
 
                     <div class="form-group form-group-edit col-xs-12 col-sm-12 col-md-12 col-lg-12" >
                         <div class="col-xs-2 col-sm-4 col-md-3 col-lg-3" style="text-align: right;">
-                            <label class="control-label labelTrack" for="taskProjectId">Task</label>
+                            <label class="control-label labelTrack" for="trackTaskId">Task</label>
                         </div>
                         <div class="controls col-xs-12 col-sm-8 col-md-9 col-lg-9">
 
-                            <select name="project_id" class="inputTrackPadding focused my_input"  id="taskProjectId"  style="height: 35px;" required>
+                            <select name="task_id" class="inputTrackPadding focused my_input"  id="trackTaskId"  style="height: 35px;" required>
                             </select>
                         </div>
                     </div>
 
                     <div class="form-group form-group-edit col-xs-12 col-sm-12 col-md-12 col-lg-12" >
                         <div class="col-xs-2 col-sm-4 col-md-3 col-lg-3" style="text-align: right;">
-                            <label class="control-label labelTrack" for="" >Start</label>
+                            <label class="control-label labelTrack" for="formTrackStart" >Start</label>
                         </div>
+
                         <div class="controls col-xs-12 col-sm-8 col-md-9 col-lg-9">
                             <div class="col-md-4 col-lg-4" style="padding: 0px">
                             <span class="input-group" >
-                                <input type="time" style="width: 70%" class="inputTrackPadding form-control" name="usr_time">
+                                <input type="text" style="width: 70%" class="inputTrackPadding form-control" name="date_start" id="formTrackStart" placeholder="HH:MM" />
+
                                 <span class="input-group-btn" style=" float:left ">
-                                    <button type="button" class="btn btn-default" style="padding:6px 1px">now</button>
-                                    <button type="button" class="btn btn-default" style="padding:6px 3px">+</button>
-                                    <button type="button" class="btn btn-default" style="padding:6px 3px">-</button>
+                                    <button type="button" class="btn btn-default" id="formTrackStartNow" style="padding:6px 1px">now</button>
+                                    <button type="button" class="btn btn-default" id="formTrackStartInc" style="padding:6px 3px">+</button>
+                                    <button type="button" class="btn btn-default" id="formTrackStartDec" style="padding:6px 3px">-</button>
                                 </span>
                             </span>
                             </div>
+
                             <div class="col-sm-12 col-md-2 col-lg-2" style="padding: 0px; text-align: center;">
-                            <label class="labelTrack">Finish</label>
-                                </div>
+                                <label class="labelTrack" for="formTrackFinish">Finish</label>
+                            </div>
+
                             <div class="col-md-4 col-lg-4" style="padding: 0px">
                             <span class="input-group" >
-                                <input type="time"  style="width: 70%; " class="inputTrackPadding form-control" name="usr_time">
+                                <input type="time"  style="width: 70%; " class="inputTrackPadding form-control" name="date_finish" id="formTrackFinish" placeholder="HH:MM">
                                 <span class="input-group-btn" style=" float:left ">
-                                    <button type="button" class="btn btn-default" style="padding:6px 1px">now</button>
-                                    <button type="button" class="btn btn-default" style="padding:6px 3px">+</button>
-                                    <button type="button" class="btn btn-default" style="padding:6px 3px">-</button>
+                                    <button type="button" class="btn btn-default" id="formTrackFinishNow" style="padding:6px 1px">now</button>
+                                    <button type="button" class="btn btn-default" id="formTrackFinishInc" style="padding:6px 3px">+</button>
+                                    <button type="button" class="btn btn-default" id="formTrackFinishDec" style="padding:6px 3px">-</button>
                                 </span>
                             </span>
                             </div>
@@ -98,45 +103,34 @@
                             <div class="col-md-2 col-lg-2" style="padding: 0px">
                                <span class="" style="display: inline-block">
                                 <label class = "labelTrack">
-                                    <input type="checkbox"> Next Day
+                                    <input type="checkbox" id="nextDay" name="nextDate"> Next Day
                                 </label>
                              </span>
                             </div>
 
-                               <!-- </div>
-
-                            <span>
-                                <label class="labelTrack" for="">Finish</label>
-                                <input type="time" style="padding: 10px" class="inputTrackPadding" name="usr_time">
-                            </span>
-                            <span class="" style="display: inline-block">
-                                <label class = "labelTrack">
-                                    <input type="checkbox"> Next Day
-                                </label>
-                             </span>-->
                         </div>
                     </div>
 
                     <div class="form-group form-group-edit col-xs-12 col-sm-12 col-md-12 col-lg-12" >
                         <div class="col-xs-2 col-sm-4 col-md-3 col-lg-3" style="text-align: right;">
-                            <label class="control-label labelTrack" for="taskProjectId" style="text-align: left; padding-top: 10px">Duration</label>
+                            <label class="control-label labelTrack" for="timeDuration" style="text-align: left; padding-top: 10px">Duration</label>
                         </div>
                         <div class="controls col-xs-12 col-sm-8 col-md-9 col-lg-9">
-                            <input type="text" style="padding: 10px; max-width: 65%; "   class="inputTrackPadding focused my_input" name="usr_time">
+                            <input type="text" style="padding: 10px; max-width: 65%; "   class="inputTrackPadding focused my_input" name="duration" id="timeDuration" placeholder="HH:MM">
                             <label class="labelTrack" for="" style="padding-top: 10px">Value($)</label>
                         </div>
                     </div>
 
-
                     <div class="form-group form-group-edit col-xs-12 col-sm-12 col-md-12 col-lg-12" >
                         <div class="col-xs-2 col-sm-4 col-md-3 col-lg-3" style="text-align: right;">
-                            <label class="control-label labelTrack" for="">Additional Cost</label>
+                            <label class="control-label labelTrack" for="additionalCost">Additional Cost</label>
                         </div>
                         <div class="controls col-xs-12 col-sm-8 col-md-9 col-lg-9">
-                            <input type="number" steep="0.01" style="padding: 10px; max-width: 65%;" class="inputTrackPadding focused my_input " name="usr_time">
-                             <span class="" style="display: inline-block">
-                                <label  class="labelTrack" >
-                                   Billable Time <input type="checkbox">
+                            <input type="number" steep="0.01" style="padding: 10px; max-width: 65%;" class="inputTrackPadding focused my_input " name="additional_cost" id="additionalCost">
+
+                            <span class="" style="display: inline-block">
+                                <label  class="labelTrack" for="billableTime">
+                                   Billable Time <input type="checkbox" name="Billable_time" id="billableTime">
                                 </label>
                              </span>
                         </div>
@@ -144,10 +138,10 @@
 
                     <div class="form-group form-group-edit col-xs-12 col-sm-12 col-md-12 col-lg-12" >
                         <div class="col-xs-2 col-sm-4 col-md-3 col-lg-3" style="text-align: right;">
-                            <label class="control-label labelTrack" for="">Description</label>
+                            <label class="control-label labelTrack" for="trackDescription">Description</label>
                         </div>
                         <div class="controls col-xs-12 col-sm-8 col-md-9 col-lg-9">
-                           <textarea class="inputTrackPadding focused my_input " rows="7"></textarea>
+                           <textarea class="inputTrackPadding focused my_input " rows="7" name="description" id="trackDescription"></textarea>
                         </div>
                     </div>
 
